d3610e85fda326688448a2dbb8f2e81666f7bd6f
Artem Strelnikov <piramida1237@gmail.com>
2025-09-29 21:31:29 +0200

    task-6_dun

--- js/task-1.js
+++ js/task-1.js
@@ -125,11 +125,12 @@
 
 // Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
 
-let counter = 20;
+let counter = 0;
 
-while (condition) {}
-
-console.log();
+while (counter <= 20) {
+  console.log(counter);
+  counter++;
+}
 
 //? task-7//
 
