542e2b36f42f602f14db3ed6400ba7aa16cca3e1
Artem Strelnikov <piramida1237@gmail.com>
2025-10-04 20:04:25 +0200

    creat _ great

--- js/task-1.js
+++ js/task-1.js
@@ -202,12 +202,15 @@
 
 function fizzBuzz(num) {
   for (let i = 1; i < num; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
       console.log("fizz");
     } else if (i % 5 === 0) {
       console.log("buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
     }
   }
 }
